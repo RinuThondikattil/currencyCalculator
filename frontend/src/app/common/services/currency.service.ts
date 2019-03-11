a73309352fc162c1e97aab61d69831c560b72ae1
RinuThondikattil <48370590+RinuThondikattil@users.noreply.github.com>
2019-03-11 08:06:51 -0400

    Update currency.service.ts

--- frontend/src/app/common/services/currency.service.ts
+++ frontend/src/app/common/services/currency.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { Http,Response,RequestOptions,Headers} from '@angular/http';
 import { Observable } from 'rxjs';
 import { map,catchError} from 'rxjs/operators';
-import { inputRate } from '../model/inputRate';
-
+import { inputRate } from '../model/inputRate.interface';
 @Injectable()
 export class CurrencyService {
   
   _url:string='http://localhost:8080/api/v1/currency';
-_currency=new inputRate
+_currency:inputRate
   constructor( private _httpService: Http){}
 
-  convertCurrency(amount,currencytype){
+  convertCurrency(amount,currencytype): Observable<JSON> {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    this._currency.amount=amount;
+   this._currency={
+     amount: amount,
+      type: currencytype
+    }
     this._currency.type=currencytype;
     return this._httpService.post(this._url, this._currency, options).pipe(map((response: Response)=>response.json()),catchError(this.handleError));
   }
